01bf5a21f03ddcd6013f0c0491e369680a304eb0
MaViMoraes <mvmoraes81@gmail.com>
2025-01-09 12:16:36 -0300

    Rep.JAVA

---
+++ secao1/HelloWord.java
@@ -0,0 +1,12 @@
+package JAVA.secao1;
+
+/*
+ * Hello Word
+ */
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+
+}
---
+++ secao2/Variaveis.java
@@ -0,0 +1,10 @@
+package JAVA.secao2;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        // 1 - O que são variaveis
+        String nome = "Talita";
+
+        System.out.println(nome);
+    }
+}
